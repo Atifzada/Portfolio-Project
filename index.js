9ca050fd16863acaf596aa54632555083fe8f65f
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-03-07 16:31:25 +0500

    Remove all the linters errors

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-let hamgurger = document.querySelector('.hamburger-icon')
-let menuItems = document.querySelector('.menu-items')
-let btnClose = document.querySelector('.close')
-let menuItem = document.querySelectorAll('.menu-item')
-hamgurger.addEventListener('click', function(){
-    console.log(hamgurger);
-    if(menuItems.style.display == 'none' || btnClose.style.display == 'none'){
-        menuItems.style.display ='flex';
-        btnClose.style.display = 'block';
-
-    }
-    else{
-         menuItems.style.display = 'none'
-    }
-})
-btnClose.addEventListener('click', function(){
-    if(menuItems.style.display == 'flex'){
-        menuItems.style.display = 'none'
-        btnClose.style.display = 'none'
-    }
-})
-for(let i=0; i < menuItem.length; i++){
-    menuItem[i].onclick = function(e){
-        menuItems.style.display = 'none'
-        btnClose.style.display = 'none'
-        location.target()
-    }
+/* eslint-disable no-restricted-globals */
+const hamgurger = document.querySelector('.hamburger-icon');
+const menuItems = document.querySelector('.menu-items');
+const btnClose = document.querySelector('.close');
+const menuItem = document.querySelectorAll('.menu-item');
+hamgurger.addEventListener('click', () => {
+  if (menuItems.style.display === 'none' || btnClose.style.display === 'none') {
+    menuItems.style.display = 'flex';
+    btnClose.style.display = 'block';
+  } else {
+    menuItems.style.display = 'none';
+  }
+});
+btnClose.addEventListener('click', () => {
+  if (menuItems.style.display === 'flex') {
+    menuItems.style.display = 'none';
+    btnClose.style.display = 'none';
+  }
+});
+for (let i = 0; i < menuItem.length; i += 1) {
+  menuItem[i].onclick = () => {
+    menuItems.style.display = 'none';
+    btnClose.style.display = 'none';
+    location.target();
+  };
 }
-
